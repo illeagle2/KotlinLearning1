a65f88d626692179ef5152d03a7ddfbd15f67d88
illeagle2 <mdilleagle@gmail.com>
2023-02-08 09:03:50 -0500

    what are secondary constructors?

--- app/src/main/java/com/example/mykotlinlearning1/Constructors.kt
+++ app/src/main/java/com/example/mykotlinlearning1/Constructors.kt
@@ -1,20 +1,38 @@
 package com.example.mykotlinlearning1
 
 /*
-* example of primary and secondary constructors
+    example of primary and secondary constructors
  */
 
 
 //primary  constructor
-class Add(val a: Int, val b: Int){
-    //some code
+
+fun main() {
+    Add(1, 2)
+    Add2(1,2)
+    Add2(1,2,3)
+    Add2(1,2,3,4)
+}
+class Add (a: Int, b: Int) {
+    var c = a + b
+
 }
 
 //Secondary constructor
 //requires constructor keyword
-class Add2(val name: String){
+//workd like overloading a class
+class Add2{
     constructor(a: Int, b:Int) {
-       var c = a + b
+       val c = a + b
        println(c)
     }
+    constructor(a:Int,b:Int,c:Int){
+        val d = a + b + c
+        println(d)
+    }
+    constructor(a:Int, b:Int,c:Int,d:Int){
+        val e = a + b + c + d
+        println(e)
+    }
 }
+
